92aeacf2ddafccc6461c06cef8525bf9f98e284f
HeXuM <hexum03@gmail.com>
2026-08-19 01:49:13 +0530

    Pipeline Fixes 4

--- lib/Utils.py
+++ lib/Utils.py
@@ -9,7 +9,6 @@ def get_spark_session(env):
         return SparkSession.builder \
             .master("local[2]") \
             .config("spark.driver.extraJavaOptions", "-Dlog4j2.configurationFile=file:log4j2.properties") \
-            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.2.0") \
             .getOrCreate()
     else:
         return SparkSession.builder \
