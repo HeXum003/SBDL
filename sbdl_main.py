b87b50c3b835971ea8baaccc5ab0a3e517261151
HeXuM <hexum03@gmail.com>
2026-08-19 01:18:26 +0530

    final code changes#3

--- sbdl_main.py
+++ sbdl_main.py
@@ -16,20 +16,3 @@ if __name__ == '__main__':
         log = Log4j2(spark)
         conf = Configloader.get_config(env_name)
         final_df = Transformation.transformations(spark, env_name)
-        # log.info("Preparing to send data to Kafka")
-        # kafka_df = final_df.select(col("payload.contractIdentifier.newValue").alias("key"), to_json(struct("*")).alias("value"))
-        #
-        # api_key = conf["kafka.api_key"]
-        # api_secret = conf["kafka.api_secret"]
-        #
-        # kafka_df.write.format("kafka") \
-        #             .option("kafka.bootstrap.servers", conf["kafka.bootstrap.servers"]) \
-        #             .option("topic", conf["kafka.topic"]) \
-        #             .option("kafka.security.protocol", conf["kafka.security.protocol"]) \
-        #             .option("kafka.sasl.jaas.config", conf["kafka.sasl.jaas.config"].format(api_key, api_secret)) \
-        #             .option("kafka.sasl.mechanism", conf["kafka.sasl.mechanism"]) \
-        #             .option("kafka.ssl.truststore.type", conf["kafka.ssl.truststore.type"]) \
-        #             .option("kafka.ssl.truststore.location", conf["kafka.ssl.truststore.location"]) \
-        #             .save()
-        #
-        # log.info("Finished sending data to Kafka")
